71f2d1ab74d0e40041a05ba533e79db2f8a8c708
little124666384 <54761569+little124666384@users.noreply.github.com>
2019-11-17 16:23:03 +0800

    20191117下午

--- 2d-un game/Assets/IT/Dog.cs
+++ 2d-un game/Assets/IT/Dog.cs
@@ -7,32 +7,39 @@ public class Dog : MonoBehaviour
     ///欄位類型 欄位名稱 值(指定 值)無的話為0 結束打;
     ///修飾詞 類型 欄位名稱 值(指定 值) 結束
     ///private私人(不顯示)public公開(顯示)
-    [Header("跳躍次數")][Range( 1, 10)]
+    [Header("跳躍次數")] [Range(1, 10)]
     public int jumpCount = 2;                                ///int 整數類別
-    [Header("跳躍高度")][Range( 1, 1000)]
+    [Header("跳躍高度")] [Range(1, 1000)]
     public int jump = 500;
-    [Header("移動速度")][Range( 0.01f, 10)]
+    [Header("移動速度")] [Range(0.01f, 10)]
     public float speed = 6.5f;                              ///float 浮點類別
-    [Header("是否在地板上")][Tooltip("用來判斷角色是否在地板上")]
+    [Header("是否在地板上")] [Tooltip("用來判斷角色是否在地板上")]
     public bool isGround;                                    ///bool  布林類別 值為開關true/flase
     [Header("角色名稱")]                                    ///public Transform dog, cam; 
     public string characterName = "Little";                 ///public Transform dog; 狗
-    public Transform dog, cam;                              ///public Transform cam; 攝影機///這兩段的精寫
-    public Animator Anim;                                   ///動畫控制器                    
-    public CapsuleCollider2D CC2D;
-    public Rigidbody2D r2d;
-    
-   #endregion
-   
+    public AudioClip soundjump, soundSlide;
+
+    private Transform cam;                              ///public Transform cam; 攝影機///這兩段的精寫
+    private Animator Anim;                                   ///動畫控制器                    
+    private CapsuleCollider2D CC2D;
+    private Rigidbody2D r2d;                                //跳耀控制代碼001
+    private AudioSource Aud;
+    #endregion
+
     #region 事件
 
     /// 初始事件遊戲開始執行一次
-    private void Start()                           
+    private void Start()
     {
-       // print("開始");
-       
-        
-        
+        // print("開始");
+        //GetComponent<T>泛型方法<T>
+        Anim = GetComponent<Animator>();
+        CC2D = GetComponent<CapsuleCollider2D>();
+        r2d = GetComponent<Rigidbody2D>();
+        cam = GameObject.Find("Main Camera").transform;
+        Aud = GetComponent<AudioSource>();
+
+
     }
     ///更新事件每桢執行一次
     private void Update()
@@ -44,21 +51,42 @@ public class Dog : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D collision)
     {
         //小刮弧的布林值為true事會執行{}內的描述
-        if (collision.gameObject.name=="地板")
+        if (collision.gameObject.name == "地板")
         {
             isGround = true;
 
         }
     }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+    
+        if (collision.name == "障礙物")
+        {
+            Damamg();
+        }
+        
+    }
+    
     #endregion
 
     #region 方法
+    /// <summary>
+    /// 角色受傷
+    /// </summary>
+    private void Damamg()
+    {
+        Debug.Log("受傷");
+    }
+
+
+
     /// <summary>
     /// 狗移動方法
     /// </summary>
     private void MoveDog()
     {                                                                               ///Translate()為移動控制指令
-        dog.Translate(speed * Time.deltaTime, 0, 0);
+        //dog.Translate(speed * Time.deltaTime, 0, 0);
+        transform.Translate(speed * Time.deltaTime, 0, 0);
     }
     /// <summary>
     /// Camear移動方法
@@ -73,12 +101,14 @@ public class Dog : MonoBehaviour
     /// </summary>
     public void Jump()
     {
+        //如果在地板上布林值=勾選
         if (isGround==true)
         {
           print("跳要");
           Anim.SetBool("跳要開關", true);
-          r2d.AddForce(new Vector2(0,jump));
-
+          r2d.AddForce(new Vector2(0,jump));           //跳耀控制代碼001
+            isGround = false;                          //地板布林值=地板
+            Aud.PlayOneShot(soundjump);
         }
     }
     /// <summary>
@@ -90,6 +120,7 @@ public class Dog : MonoBehaviour
         Anim.SetBool("滑行開關", true);
         CC2D.offset=new Vector2(-0.1f, -1.044564f);
         CC2D.size = new Vector2(0.95f, 1.84102f);
+        Aud.PlayOneShot(soundSlide,10);
     }
     /// <summary>
     /// 重設定跳耀與滑行布林值,從新設定碰撞器
